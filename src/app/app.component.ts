8cfdb80699a3890e4fb5b9feff5ca90914b2d05d
alejandraxxxx <xagacia.03@gmail.com>
2024-09-30 16:16:30 -0600

    creando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterModule, TodoListComponent],
+  template : `<app-todo-list></app-todo-list>`,
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'todo-app';
